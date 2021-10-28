74e7a949ccb34edadf2c5d7b287d922b98a0d0b5
JadonLee8 <jadonlee888@gmail.com>
2021-10-27 19:18:12 -0500

    10/27/2021: finished drivetrain code for mecanum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Iterative.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Iterative.java
@@ -35,6 +35,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+import java.util.*;
 
 /**
  * This file contains an example of an iterative (Non-Linear) "OpMode".
@@ -51,14 +52,15 @@ import com.qualcomm.robotcore.util.Range;
  */
 
 @TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
-@Disabled
+
 public class BasicOpMode_Iterative extends OpMode
 {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
-
+    private DcMotor leftFrontWheel = null;
+    private DcMotor rightFrontWheel = null;
+    private DcMotor leftBackWheel = null;
+    private DcMotor rightBackWheel = null;
     /*
      * Code to run ONCE when the driver hits INIT
      */
@@ -69,13 +71,17 @@ public class BasicOpMode_Iterative extends OpMode
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        leftFrontWheel  = hardwareMap.get(DcMotor.class, "left_front");
+        rightFrontWheel = hardwareMap.get(DcMotor.class, "right_front");
+        leftBackWheel = hardwareMap.get(DcMotor.class, "left_back");
+        rightBackWheel = hardwareMap.get(DcMotor.class, "right_back");
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftFrontWheel.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontWheel.setDirection(DcMotor.Direction.REVERSE);
+        leftBackWheel.setDirection(DcMotor.Direction.FORWARD);
+        rightBackWheel.setDirection(DcMotor.Direction.REVERSE);
 
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
@@ -102,30 +108,54 @@ public class BasicOpMode_Iterative extends OpMode
     @Override
     public void loop() {
         // Setup a variable for each drive wheel to save power level for telemetry
-        double leftPower;
-        double rightPower;
+        double leftFrontPower;
+        double rightFrontPower;
+        double leftBackPower;
+        double rightBackPower;
 
         // Choose to drive using either Tank Mode, or POV Mode
         // Comment out the method that's not used.  The default below is POV.
 
         // POV Mode uses left stick to go forward, and right stick to turn.
         // - This uses basic math to combine motions and is easier to drive straight.
-        double drive = -gamepad1.left_stick_y;
-        double turn  =  gamepad1.right_stick_x;
-        leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-        rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+        double forwardBackward = -gamepad1.left_stick_y;
+        double straifing =  gamepad1.left_stick_x;
+        double turning = gamepad1.right_stick_x;
 
-        // Tank Mode uses one stick to control each wheel.
-        // - This requires no math, but it is hard to drive forward slowly and keep straight.
+
+
+
+
+
+
+
+
+        //TODO: replace range.clip with a method that divides all of the joysitck values above the absolute value of one down proportionally
+        leftFrontPower    = forwardBackward + turning + straifing;
+        rightFrontPower   = forwardBackward - turning - straifing;
+        leftBackPower     = forwardBackward + turning + straifing;
+        rightBackPower    = forwardBackward - turning - straifing;
+        double[] powVals = {abs(leftFrontPower), abs(rightFrontPower), abs(leftBackPower), abs(rightBackPower)};
+        Arrays.sort(powVals);
+        if ((abs(leftFrontPower)) > 1 || (abs(rightFrontPower)) > 1 || (abs(leftBackPower) > 1) || (abs(rightBackPower) > 1))    {
+             double maxPower = powVals[3];
+             leftFrontPower    /= maxPower;
+             rightFrontPower   /= maxPower;
+             leftBackPower     /= maxPower;
+             rightBackPower    /= maxPower;
+              }
         // leftPower  = -gamepad1.left_stick_y ;
         // rightPower = -gamepad1.right_stick_y ;
 
         // Send calculated power to wheels
-        leftDrive.setPower(leftPower);
-        rightDrive.setPower(rightPower);
+        leftFrontWheel.setPower(leftFrontPower);
+        rightFrontWheel.setPower(rightFrontPower);
+        leftBackWheel.setPower(leftBackPower);
+        rightBackWheel.setPower(rightBackPower);
 
         // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
+                            //TODO: fix the following line to work with mecanum
         telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
     }
 
@@ -134,6 +164,9 @@ public class BasicOpMode_Iterative extends OpMode
      */
     @Override
     public void stop() {
+        leftFrontWheel.setPower(0);
+        rightFrontWheel.setPower(0);
+        leftBackWheel.setPower(0);
+        rightBackWheel.setPower(0);
     }
-
 }
