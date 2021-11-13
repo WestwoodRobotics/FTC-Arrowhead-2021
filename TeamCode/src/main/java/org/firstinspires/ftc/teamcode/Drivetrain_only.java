d40b2c355dd5bf31fd6c97532a169bd9e8262e87
Christopher Li <90669504+empebeetle@users.noreply.github.com>
2021-11-13 07:06:24 -0600

    Update Drivetrain_only.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain_only.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain_only.java
@@ -12,25 +12,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 import java.util.Arrays;
-/*
-TODOs:
-remove @overides
- */
 
-
-/**
- * This file contains an example of an iterative (Non-Linear) "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all iterative OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
 
 @TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
 
@@ -144,11 +126,11 @@ public class DrivetrainOnly extends OpMode
         }
 
 
-        if(gamepad2.left_bumper == true) {
+        if(gamepad2.right_bumper == true) {
             holderServo.setPosition(0.5);
             }
-        else if(gamepad2.right_bumper == true){
-            holderServo.setPosition(0.0);
+        else if(gamepad2.left_bumper == true){
+            holderServo.setPosition(1.0);
             }
         
         
@@ -165,8 +147,8 @@ public class DrivetrainOnly extends OpMode
         else if (gamepad2.dpad_right) {
             elevatorMotor.setPower(0.25);
         }
-        else {
-            elevatorMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        else if (gamepad2.x) {
+            elevatorMotor.setPower(0.0);
         }
         
         
